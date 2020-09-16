f1102afac6f10ca9e488cbded7c0413e6db40150
ahmad.basori <ahmad.basori@binasanprima.com>
2020-09-16 14:31:37 +0700

    BIMySQL API Production Initial Commit

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,16 +12,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//Route::get('students2','StudentsController@students2');
+//Route::get('getstock_move_line/{no_delivery}','ApiOdooController@get_rqStockMoveLine');
+//Route::get('bismysql/getStockMoveLineRow/{picking_id}','BISAPIController@getStockMoveLineRow');
 
- //Route::get('students2','StudentsController@students2');
- //Route::get('getstock_move_line/{no_delivery}','ApiOdooController@get_rqStockMoveLine');
- #Route::get('bismysql/getStockMoveLineRow/{picking_id}','BISAPIController@getStockMoveLineRow');
- Route::get('bismysql/getUpdatePickingItem/{picking_id}','BISAPIController@getUpdatePickingItem');  
- Route::get('bismysql/sentStockAdjusment/{adjustment_id}','BISAPIController@sentStockAdjusment');   
- Route::get('bismysql/getSessionID','BISAPIController@getSessionID');  
- Route::get('bismysql/kartuStok/{no_kertas_kerja}','BISAPIController@kartuStok');  
- 
- Route::get('stockopname','StockOpnameController@index');
- Route::post('stockopname','StockOpnameController@store');
- 
+//route module stock picking
+Route::get('bismysql/getUpdatePickingItem/{picking_id}','BISAPIController@getUpdatePickingItem');  
+Route::get('bismysql/sentStockAdjusment/{adjustment_id}','BISAPIController@sentStockAdjusment');   
+Route::get('bismysql/getSessionID','BISAPIController@getSessionID');  
 
+//route module kartu stock
+Route::get('bismysql/kartuStok/{no_kertas_kerja}','BISAPIController@kartuStok');  
+Route::get('stockopname','StockOpnameController@index');
+Route::post('stockopname','StockOpnameController@store');
