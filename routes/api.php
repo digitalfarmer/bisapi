1cbe2a6eba9f0f1ff072ef6b36a4b1670a68ce47
ahmad.basori <ahmad.basori@binasanprima.com>
2020-09-30 11:11:01 +0700

    automation test

--- routes/api.php
+++ routes/api.php
@@ -14,17 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
  
 //route module stock picking BLG
- Route::get('blg/getUpdatePickingItem/{picking_id}','BISAPIController@getUpdatePickingItem');   
+Route::get('blg/getUpdatePickingItem/{picking_id}','BISAPIController@getUpdatePickingItem');   
 //route module Stock Adjustment BLG
- Route::get('blg/sendStockAdjustment/{adjustment_id}','ProsesKartuStokController@sendStockAdjustment');   
- Route::get('blg/getStockOpname/{adjustment_id}','ProsesKartuStokController@getStockOpname'); 
- Route::get('blg/get_stock/{adjustment_id}','ProsesKartuStokController@get_stock');     
- Route::post('blg/flagBlockingStock/{adjustment_id}','ProsesKartuStokController@FlagBlockingStock');   
+Route::get('blg/sendStockAdjustment/{adjustment_id}','ProsesKartuStokController@sendStockAdjustment');   
+Route::get('blg/getStockOpname/{adjustment_id}','ProsesKartuStokController@getStockOpname'); 
+Route::get('blg/get_stock/{adjustment_id}','ProsesKartuStokController@get_stock');     
+Route::post('blg/flagBlockingStock/{adjustment_id}','ProsesKartuStokController@FlagBlockingStock');   
  //Cek Status Adjustment BLG
- Route::post('blg/cekOpnameStatus','BISMySQLController@cekOpnameStatus');
- Route::get('blg/cekDivisiProdukOpname/{Kode_Divisi_Produk}','BISMySQLController@cekDivisiProdukOpname');
+Route::post('blg/cekOpnameStatus','BISMySQLController@cekOpnameStatus');
+Route::get('blg/cekDivisiProdukOpname/{Kode_Divisi_Produk}','BISMySQLController@cekDivisiProdukOpname');
  //BISMySQL numbering BLG
- Route::post('blg/getNewNumber/{type_nomor}','BISMySQLController@getNewNumber');
+Route::post('blg/getNewNumber/{type_nomor}','BISMySQLController@getNewNumber');
  //create kkso BLG
 Route::get('blg/createadjustment','StockOpnameController@createkkso');
-//------------2020-09-30
+//------------2020-09-30 11:05
+
+//test automation
